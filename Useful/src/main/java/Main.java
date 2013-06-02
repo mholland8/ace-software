90dfc93a00346a6c24fc7904a4a0d880026a9c80
mholland8 <mark@markholland.me.uk>
2013-06-02 21:26:03 +0100

    Close application context on exit. Added formatterand save on exit hooks

--- Useful/src/main/java/Main.java
+++ Useful/src/main/java/Main.java
@@ -5,43 +5,50 @@ import javax.jms.TextMessage;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.useful.jdbc.DBReader;
 
+public class Main implements MessageListener
+{
 
-public class Main implements MessageListener {
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
-	private static final Logger logger = LogManager.getLogger(Main.class);
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		logger.info("Startup...");
-		
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
-		
-		logger.info("Spring Container initialised");
-		
-		
-		DBReader reader = (DBReader)appContext.getBean("dbReader");
-		reader.runQuery();
-	}
-	public void onMessage(Message arg0) {
-		if (arg0 instanceof TextMessage)
-		{
-			TextMessage tm = (TextMessage)arg0;
-			try {
-				logger.info(tm.getText());
-			} catch (JMSException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		else
-		{
-			logger.warn("received unknown message type");
-		}
-	}
+    /**
+     * @param args
+     */
+    public static void main(final String[] args)
+    {
+        logger.info("Startup...");
+
+        ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
+
+        logger.info("Spring Container initialised");
+
+        DBReader reader = (DBReader) appContext.getBean("dbReader");
+        reader.runQuery();
+
+        appContext.close();
+    }
+
+    public void onMessage(Message arg0)
+    {
+        if (arg0 instanceof TextMessage)
+        {
+            TextMessage tm = (TextMessage) arg0;
+            try
+            {
+                logger.info(tm.getText());
+            }
+            catch (JMSException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            logger.warn("received unknown message type");
+        }
+    }
 }
