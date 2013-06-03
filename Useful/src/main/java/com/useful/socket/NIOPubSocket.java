dbe389d513c7578da4fb5ef617886095dbcddd18
mholland8 <mark@markholland.me.uk>
2013-06-03 21:29:45 +0100

    Further buildout of NIO implementation

--- Useful/src/main/java/com/useful/socket/NIOPubSocket.java
+++ Useful/src/main/java/com/useful/socket/NIOPubSocket.java
@@ -21,12 +21,15 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.spi.SelectorProvider;
 import java.util.Iterator;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -41,185 +44,229 @@ import org.apache.logging.log4j.Logger;
  */
 public class NIOPubSocket implements Runnable
 {
-    private static final Logger logger = LogManager.getFormatterLogger(NIOPubSocket.class);
-    /**
-     * Defines maximum size of client queue
-     */
-    private static final int HIGH_WATER_MARK = 1024;
+	private static final Logger logger = LogManager.getFormatterLogger(NIOPubSocket.class);
+	/**
+	 * Defines maximum size of client queue
+	 */
+	private static final int HIGH_WATER_MARK = 1024;
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args)
-    {
-        try
-        {
-            NIOPubSocket s = new NIOPubSocket();
-            s.bind(5555);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			NIOPubSocket s = new NIOPubSocket();
+			s.bind(5555);
 
-            while (true)
-            {
-                byte[] bytes = "Hello World".getBytes();
-                s.send(bytes);
-            }
-        }
-        catch (IOException ex)
-        {
-            ex.printStackTrace();
-        }
-    }
+			while (true) {
+				byte[] bytes = "Hello World".getBytes();
+				s.send(bytes);
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
-    public NIOPubSocket() throws IOException
-    {
-        // Create a new non-blocking server socket channel
-        serverChannel = ServerSocketChannel.open();
-        serverChannel.configureBlocking(false);
-        serverChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+	/**
+	 * Create a new pub socket using NIO library to read and write messages
+	 * asynchronously
+	 * 
+	 * @throws IOException
+	 */
+	public NIOPubSocket() throws IOException {
+		// Create a new non-blocking server socket channel
+		serverChannel = ServerSocketChannel.open();
+		serverChannel.configureBlocking(false);
+		serverChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
 
-        scheduler.scheduleAtFixedRate(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                logger.info("Outbound message rate : %d per second", numMessages.getAndSet(0));
-            }
-        }, 1000, 1000, TimeUnit.MILLISECONDS);
-    }
+		scheduler.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				logger.info("Outbound message rate : %d per second",
+						numMessages.getAndSet(0));
+			}
+		}, 1000, 1000, TimeUnit.MILLISECONDS);
+	}
 
-    public void bind(int port) throws IOException
-    {
-        // Bind the server socket to the specified address and port
-        InetSocketAddress isa = new InetSocketAddress(port);
-        serverChannel.socket().bind(isa);
+	/**
+	 * Bind the socket to a known port
+	 * 
+	 * @param port
+	 * @throws IOException
+	 */
+	public void bind(int port) throws IOException {
+		// Bind the server socket to the specified address and port
+		InetSocketAddress isa = new InetSocketAddress(port);
+		serverChannel.socket().bind(isa);
 
-        // Create a new selector
-        selector = SelectorProvider.provider().openSelector();
+		// Create a new selector
+		selector = SelectorProvider.provider().openSelector();
 
-        // Register the server socket channel, indicating an interest in
-        // accepting new connections
-        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+		// Register the server socket channel, indicating an interest in
+		// accepting new connections
+		serverChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-        // now we are bound to a port we can begin listening for connections
-        // from clients
-        new Thread(this).start();
-    }
+		// now we are bound to a port we can begin listening for connections
+		// from clients
+		new Thread(this).start();
+	}
 
-    public void close() throws IOException
-    {
-        /*
-         * for (ClientConnection c : clients) { c.close(); } clients.clear();
-         * 
-         * if (serverSocket != null) { // close the server socket but do not
-         * delete // this allows us to re-open if desired serverSocket.close();
-         * }
-         */
-    }
+	/**
+	 * Close the pub socket. All connected client will be disconnected.
+	 * Any pending messages will be discarded
+	 * 
+	 * @throws IOException
+	 */
+	public void close() throws IOException
+	{
+		for (ClientConnection c : clients)
+		{
+			c.close();
+		}
+		clients.clear();
 
-    public void send(byte[] bytes) throws IOException
-    {
-        /*
-         * // send copy of message to each connected client // any errors or
-         * exceptions will results in the // client being removed from our list
-         * byte[] msg = encodeMessage(bytes); for (ClientConnection c : clients)
-         * { try { // this just enqueues the message on the client queue
-         * c.send(msg);
-         * 
-         * numMessages.incrementAndGet(); } catch (Exception ex) { // client is
-         * no longer valid, drop the connection c.close(); clients.remove(c);
-         * logger.info(String.format("%d clients connected", clients.size())); }
-         * }
-         */
-    }
+		if (serverChannel != null)
+		{
+			serverChannel.close();
+		}
+	}
 
-    private byte[] encodeMessage(byte[] bytes) throws IOException
-    {
-        int size = bytes.length;
-        byte[] tmpBuf = new byte[size + 4];
+	/**
+	 * send a copy of the message to each connected client. No IO is done in this method,
+	 * this just simply queues the message on the clients outbound queue and waits for the
+	 * selector to indicate when the client is ready to receive more data.
+	 * If the clients queue hits the high water mark, this method will either block
+	 * or disconnect the client depending on the slow client strategy
+	 * 
+	 * @param bytes
+	 * @throws IOException
+	 */
+	public void send(byte[] bytes) throws IOException {
+		/*
+		 * send copy of message to each connected client. Any errors or
+		 * exceptions will results in the client being removed from our list
+		 */
+		byte[] msg = encodeMessage(bytes);
+		for (ClientConnection c : clients) {
+			try {
+				// this just enqueues the message on the client queue
+				c.send(msg);
 
-        tmpBuf[3] = (byte) (size & 0xff);
-        size = size >> 8;
-        tmpBuf[2] = (byte) (size & 0xff);
-        size = size >> 8;
-        tmpBuf[1] = (byte) (size & 0xff);
-        size = size >> 8;
-        tmpBuf[0] = (byte) (size & 0xff);
-        size = size >> 8;
+				numMessages.incrementAndGet();
+			} catch (Exception ex) {
+				// client is no longer valid, drop the connection
+				c.close();
+				clients.remove(c);
+				logger.info(String.format("%d clients connected",
+						clients.size()));
+			}
+		}
+	}
 
-        System.arraycopy(bytes, 0, tmpBuf, 4, bytes.length);
+	private byte[] encodeMessage(byte[] bytes) throws IOException {
+		int size = bytes.length;
+		byte[] tmpBuf = new byte[size + 4];
 
-        return tmpBuf;
-    }
+		tmpBuf[3] = (byte) (size & 0xff);
+		size = size >> 8;
+		tmpBuf[2] = (byte) (size & 0xff);
+		size = size >> 8;
+		tmpBuf[1] = (byte) (size & 0xff);
+		size = size >> 8;
+		tmpBuf[0] = (byte) (size & 0xff);
+		size = size >> 8;
 
-    @Override
-    public void run()
-    {
-        while (true)
-        {
-            try
-            {
-                // Wait for an event one of the registered channels
-                selector.select();
+		System.arraycopy(bytes, 0, tmpBuf, 4, bytes.length);
 
-                // Iterate over the set of keys for which events are
-                // available
-                Iterator<SelectionKey> selectedKeys = selector.selectedKeys().iterator();
+		return tmpBuf;
+	}
 
-                while (selectedKeys.hasNext())
-                {
-                    SelectionKey key = selectedKeys.next();
-                    selectedKeys.remove();
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				// Wait for an event one of the registered channels
+				selector.select();
 
-                    if (!key.isValid())
-                    {
-                        continue;
-                    }
+				// Iterate over the set of keys for which events are
+				// available
+				Iterator<SelectionKey> selectedKeys = selector.selectedKeys()
+						.iterator();
 
-                    // Check what event is available and deal with it
-                    if (key.isAcceptable())
-                    {
-                        accept(key);
-                    }
-                    else if (key.isWritable())
-                    {
-                        write(key);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
-        
-    private void accept(SelectionKey key) throws IOException
-    {
-        // Accept the connection and make it non-blocking
-        SocketChannel socketChannel = serverChannel.accept();
-        socketChannel.configureBlocking(false);
+				while (selectedKeys.hasNext()) {
+					SelectionKey key = selectedKeys.next();
+					selectedKeys.remove();
 
-        // Register the new SocketChannel with our Selector, indicating
-        // we'd like to be notified when the socket is available for writing
-        socketChannel.register(selector, SelectionKey.OP_WRITE);
-        
-        logger.info("Accepted new connection");
-    }
-    
-    private void write(SelectionKey key) throws IOException
-    {
-    }
-    
-    private final AtomicInteger numMessages = new AtomicInteger(0);
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-    // private final CopyOnWriteArrayList<ClientConnection> clients = new
-    // CopyOnWriteArrayList<ClientConnection>();
+					if (!key.isValid()) {
+						continue;
+					}
 
-    // The channel on which we'll accept connections
-    private ServerSocketChannel serverChannel;
+					// Check what event is available and deal with it
+					if (key.isAcceptable()) {
+						accept(key);
+					} else if (key.isWritable()) {
+						write(key);
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    // The selector we'll be monitoring
-    private Selector selector;
+	private void accept(SelectionKey key) throws IOException
+	{
+		// Accept the connection and make it non-blocking
+		SocketChannel socketChannel = serverChannel.accept();
+		socketChannel.configureBlocking(false);
 
-    // The buffer into which we'll read data when it's available
-    private ByteBuffer readBuffer = ByteBuffer.allocate(8192);
+		// Register the new SocketChannel with our Selector, indicating
+		// we'd like to be notified when the socket is available for writing
+		socketChannel.register(selector, SelectionKey.OP_WRITE);
+
+		logger.info("Accepted new connection");
+	}
+
+	private void write(SelectionKey key) throws IOException {
+		SelectableChannel channel = key.channel();
+	}
+
+	private class ClientConnection {
+		ClientConnection(SocketChannel clientChannel) {
+			channel = clientChannel;
+		}
+
+		void close() {
+			try {
+				channel.close();
+			} catch (Exception ex) {
+				if (logger.isDebugEnabled())
+					logger.debug(ex);
+			}
+		}
+
+		void send(byte[] bytes) throws InterruptedException, IOException {
+			if (channel.isOpen()) {
+				outboundQueue.put(bytes);
+			} else
+				throw new IOException("Socket closed");
+		}
+
+		private final ArrayBlockingQueue<byte[]> outboundQueue = new ArrayBlockingQueue<byte[]>(
+				HIGH_WATER_MARK);
+		private final SocketChannel channel;
+	}
+
+	private final AtomicInteger numMessages = new AtomicInteger(0);
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+	private final CopyOnWriteArrayList<ClientConnection> clients = new CopyOnWriteArrayList<ClientConnection>();
+
+	// The channel on which we'll accept connections
+	private final ServerSocketChannel serverChannel;
+
+	// The selector we'll be monitoring
+	private Selector selector;
+
+	// The buffer into which we'll read data when it's available
+	private final ByteBuffer readBuffer = ByteBuffer.allocate(8192);
 }
