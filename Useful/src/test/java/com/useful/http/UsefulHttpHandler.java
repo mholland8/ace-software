63fd6a6742f939092d3d89d3ca8bb5697c7b5e8d
mholland8 <mark@markholland.me.uk>
2013-07-07 17:59:21 +0100

    Added Apache Camel HTTP handler

--- Useful/src/test/java/com/useful/http/UsefulHttpHandler.java
+++ Useful/src/test/java/com/useful/http/UsefulHttpHandler.java
@@ -1,16 +1,17 @@
 package com.useful.http;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+import java.io.InputStream;
+import java.util.Set;
 
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.portlet.PortletFileUpload;
+import javax.activation.DataHandler;
+import javax.servlet.http.HttpServletRequest;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * http://evgenyg.wordpress.com/2010/05/01/uploading-files-multipart-post-apache
@@ -20,43 +21,57 @@ import com.sun.net.httpserver.HttpServer;
  * @author Mark
  * 
  */
-public class UsefulHttpHandler implements HttpHandler
+public class UsefulHttpHandler implements Processor
 {
-
-    @Override
-    public void handle(HttpExchange arg0) throws IOException
-    {
-        // Create a factory for disk-based file items
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        factory.setRepository(new File("C:\\temp"));
-
-        // Create a new file upload handler
-        PortletFileUpload upload = new PortletFileUpload(factory);
-
-        // Set overall request size constraint
-        // upload.setSizeMax(yourMaxRequestSize);
-
-        // Parse the request
-        // List<FileItem> items = upload.parseRequest();
-
-        String response = "<!DOCTYPE html><html><body><form method=\"POST\" enctype=\"multipart/form-data\" action=\"fup.cgi\">  File to upload: <input type=\"file\" name=\"upfile\"><br/>" + "<br/>" + "<input type=\"submit\" value=\"Press\"> to upload the file!" + "</form></body></form>";
-        arg0.sendResponseHeaders(200, response.length());
-        OutputStream os = arg0.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-    }
-
     /**
      * @param args
-     * @throws IOException
+     * @throws Exception
      */
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args) throws Exception
     {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), -1);
-        server.createContext("/applications/myapp", new UsefulHttpHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder()
+        {
+            public void configure()
+            {
+                from("jetty:http://localhost:8000/myapp/myservice").process(new UsefulHttpHandler());
+            }
+        });
+
+        context.start();
 
     }
 
+    @Override
+    public void process(Exchange exchange) throws Exception
+    {
+
+        // we have access to the HttpServletRequest here and we can grab it if
+        // we need it
+        HttpServletRequest req = exchange.getIn().getBody(HttpServletRequest.class);
+
+        // Check that we have a file upload request
+        if (!ServletFileUpload.isMultipartContent(req))
+        {
+            Set<String> attachments = exchange.getIn().getAttachmentNames();
+            for (String name : attachments)
+            {
+                System.out.println(name);
+                DataHandler hndl = exchange.getIn().getAttachment(name);
+                System.out.println(hndl.getContentType());
+
+                saveToFile(name, hndl.getInputStream());
+            }
+        }
+        else
+        {
+            System.out.println(req.toString());
+            String response = "<!DOCTYPE html><html><body><form method=\"POST\" enctype=\"multipart/form-data\" action=\"myservice\">  File to upload: <input type=\"file\" name=\"upfile\"><br/>" + "<br/>" + "<input type=\"submit\" value=\"Press\"> to upload the file!" + "</form></body></form>";
+            exchange.getOut().setBody(response);
+        }
+    }
+
+    private void saveToFile(String name, InputStream stream) throws Exception
+    {
+    }
 }
